9526902fe683190c6529a10d219f8e001dff5763
Clément Schreiner <clement@mux.me>
2023-07-26 22:40:48 +0200

    gui: set default path for db

--- gui/src-tauri/src/main.rs
+++ gui/src-tauri/src/main.rs
@@ -1,8 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use holdem_suite_parser::get_summaries;
 use holdem_suite_parser::models::Summary;
+use holdem_suite_parser::{establish_connection, get_summaries};
+
+struct Settings<'a> {
+    database_url: &'a str,
+}
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -11,8 +15,8 @@ fn greet(name: &str) -> String {
 }
 
 #[tauri::command]
-fn load_summaries() -> Vec<Summary> {
-    get_summaries()
+fn load_summaries(state: tauri::State<Settings>) -> Vec<Summary> {
+    get_summaries(state.database_url)
 }
 
 use tauri::Manager;
@@ -31,6 +35,9 @@ async fn close_splashscreen(window: tauri::Window) {
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![close_splashscreen, load_summaries])
+        .manage(Settings {
+            database_url: "sqlite:///home/clemux/dev/holdem-suite/parser/test.db",
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
