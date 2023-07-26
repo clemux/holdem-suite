0b4aff0bc585d3df6416cf0971205e54f22164fd
Clément Schreiner <clement@mux.me>
2023-07-26 22:40:55 +0200

    gui: add tabs

--- gui/src/App.vue
+++ gui/src/App.vue
@@ -1,9 +1,50 @@
 <script setup lang="ts">
+import {ref} from "vue";
 import Summaries from "./components/Summaries.vue";
+import {QSplitter, QTabs} from "quasar";
+
+const splitterModel = ref(20);
+const tab = ref('tournaments');
+
 </script>
 
 <template>
-  <div class="container">
-    <Summaries />
+  <div>
+      <q-splitter
+      v-model="splitterModel"
+    >
+
+      <template v-slot:before>
+        <q-tabs
+          v-model="tab"
+          vertical
+          class="text-teal"
+        >
+          <q-tab name="tournaments" label="Tournaments" />
+          <q-tab name="hands" label="Hands" />
+        </q-tabs>
+      </template>
+
+      <template v-slot:after>
+        <q-tab-panels
+          v-model="tab"
+          animated
+          swipeable
+          vertical
+          transition-prev="jump-up"
+          transition-next="jump-up"
+        >
+          <q-tab-panel name="tournaments">
+            <Summaries />
+          </q-tab-panel>
+
+          <q-tab-panel name="hands">
+          </q-tab-panel>
+
+
+        </q-tab-panels>
+      </template>
+
+    </q-splitter>
   </div>
 </template>
