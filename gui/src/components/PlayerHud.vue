75b1c90455dc2cb3a53689e68f9f3f76b6d41747
Clément Schreiner <clement@mux.me>
2023-08-14 01:36:46 +0200

    gui: autoload hud stats

--- gui/src/components/PlayerHud.vue
+++ gui/src/components/PlayerHud.vue
@@ -2,7 +2,7 @@
 
 import {Player, PlayerStats} from "../lib/types";
 import {invoke} from "@tauri-apps/api/tauri";
-import {ref} from "vue";
+import {onMounted, ref, watch} from "vue";
 
 const stats = ref<PlayerStats | null>(null);
 
@@ -14,25 +14,36 @@ async function loadPlayerStats() {
   stats.value = await invoke("load_player_stats", {playerName: props.player.name});
 }
 
+async function clickedTable() {
+  console.log("clicked table");
+}
+
+watch(() => props.player, async (_, __) => {
+  await loadPlayerStats();
+})
+
+onMounted(() => {
+  loadPlayerStats();
+});
+
 </script>
 
 <template>
   <div class="row">
-    <div class="q-pa-xs q-gutter-xs">
-      <q-badge>{{ player.name }}</q-badge>
-      <q-badge>{{ player.nb_hands }}</q-badge>
-    </div>
   </div>
-  <div class="row">
-    <div v-if="stats" class="q-pa-xs q-gutter-xs">
-      <q-badge rounded color="red">{{ stats.vpip.toFixed(2) }}</q-badge>
-      <q-badge rounded color="primary">{{ stats.pfr.toFixed(2) }}</q-badge>
-      <q-badge rounded color="orange">{{ stats.three_bet.toFixed(2) }}</q-badge>
-    </div>
-  </div>
-  <form class="row" @submit.prevent="loadPlayerStats">
-    <button type="submit">Load Stats</button>
-  </form>
+  <table v-if="stats" @click="clickedTable()">
+    <tr>
+      <td>{{ player.name }}</td>
+      <td>{{ player.nb_hands }}</td>
+    </tr>
+    <tr>
+      <td>
+        {{ stats.vpip.toFixed(2) }}
+      </td>
+      <td>{{ stats.pfr.toFixed(2) }}</td>
+      <td>{{ stats.three_bet.toFixed(2) }}</td>
+    </tr>
+  </table>
 </template>
 
 <style scoped>
