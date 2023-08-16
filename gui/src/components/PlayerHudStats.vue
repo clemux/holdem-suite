820b8a11f5a486e1550e7f08af68d5ee272f48da
Clément Schreiner <clement@mux.me>
2023-08-17 00:12:13 +0200

    gui: improve hud display

--- gui/src/components/PlayerHudStats.vue
+++ gui/src/components/PlayerHudStats.vue
@@ -3,9 +3,8 @@
 import {Player, PlayerStats} from "../lib/types";
 import {invoke} from "@tauri-apps/api/tauri";
 import {onMounted, ref, watch} from "vue";
-
-import { WebviewWindow } from '@tauri-apps/api/window'
 import {listen} from "@tauri-apps/api/event";
+
 const stats = ref<PlayerStats | null>(null);
 const props = defineProps<{
   player: Player;
@@ -15,28 +14,6 @@ async function loadPlayerStats() {
   stats.value = await invoke("load_player_stats", {playerName: props.player.name});
 }
 
-async function clickedTable() {
-  console.log("clicked table");
-  const webview = new WebviewWindow('Popup', {
-  url: 'hud-popup.html',
-})
-  await webview.once('tauri://created', function () {
-    console.log("Sending event");
-    webview.emit('load', { data: "cocuccocxc" })
-})
-
-  await webview.once('tauri://error', function (e) {
-  console.log(e);
-})
-}
-
-async function sendSignal() {
-  console.log("Sending signal");
-  const window = WebviewWindow.getByLabel('Popup');
-  if (window) {
-    await window.emit('load', {player: props.player});
-  }
-}
 
 async function openPopup() {
   console.log("Opening popup");
@@ -67,16 +44,16 @@ onMounted(() => {
   </div>
   <table v-if="stats" @click="openPopup()">
     <tr>
-      <td>{{ player.name }}</td>
+      <td colspan="3">{{ player.name }}</td>
       <td>{{ stats.nb_hands }}</td>
     </tr>
     <tr>
       <td>
-        {{ stats.vpip.toFixed(2) }}
+        {{ (stats.vpip * 100).toFixed(0) }}
       </td>
-      <td>{{ stats.pfr.toFixed(2) }}</td>
-      <td>{{ stats.three_bet.toFixed(2) }}</td>
-      <td>{{ stats.open_limp.toFixed(2) }}</td>
+      <td>{{ (stats.pfr * 100).toFixed(0) }}</td>
+      <td>{{ (stats.three_bet * 100).toFixed(1) }}</td>
+      <td>{{ (stats.open_limp * 100).toFixed(0) }}</td>
     </tr>
   </table>
 </template>
