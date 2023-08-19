0a3a0d2d72a3426842c1d7516ba7a3458173090f
Clément Schreiner <clement@mux.me>
2023-08-19 14:57:09 +0200

    db: remove debug print

--- db/src/lib.rs
+++ db/src/lib.rs
@@ -17,7 +17,6 @@ pub mod models;
 pub mod schema;
 
 pub fn establish_connection(database_url: &str) -> SqliteConnection {
-    println!("Connecting to {}", database_url);
     let mut connection = SqliteConnection::establish(database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
     connection
